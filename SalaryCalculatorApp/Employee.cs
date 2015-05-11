9c326f0789e3f3f4b0ec9f3c40d2d50920306e82
RahmanTarik <tarikseu@gmail.com>
2015-05-11 17:09:21 +0600

    Someone's App

---
+++ SalaryCalculatorApp/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryCalculatorApp
+{
+    class Employee
+    {
+        public string employeeName;
+        public double amount;
+        public double rent;
+        public double medicalAllowence;
+
+
+        public double CalculateSalary()
+        {
+            return (amount + (amount * rent) / 100 + (amount * medicalAllowence) / 100); 
+            
+         }
+    }
+}
---
+++ SalaryCalculatorApp/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalaryCalculatorApp
+{
+    public partial class CalculatorSalaryUI : Form
+    {
+        public CalculatorSalaryUI()
+        {
+            InitializeComponent();
+        }
+        Employee aEmployee = new Employee();
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            aEmployee.employeeName=nameTextBox.Text;
+            aEmployee.amount = Convert.ToDouble(amountTextBox.Text);
+            aEmployee.rent=Convert.ToDouble(rentTextBox.Text);
+            aEmployee.medicalAllowence = Convert.ToDouble(medicalTextBox.Text);
+
+            double salary = (aEmployee.CalculateSalary());
+            MessageBox.Show(aEmployee.employeeName + " Your total salary Is : " +salary);
+
+
+            nameTextBox.Clear();
+            rentTextBox.Clear();
+            amountTextBox.Clear();
+            medicalTextBox.Clear();
+        }
+    }
+}
